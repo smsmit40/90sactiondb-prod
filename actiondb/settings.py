2365fcd72a255c09f21131e8da4b431293049af0
smsmit40 <64119069+smsmit40@users.noreply.github.com>
2021-01-16 22:46:41 -0600

    Update settings.py

--- actiondb/settings.py
+++ actiondb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6$y3s&nwv@exno^h!fk8jkdz7gb2)imf07&jgz1^_s%8)ks0x6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['actiondb90s.herokuapp.com', '127.0.0.1']
 
