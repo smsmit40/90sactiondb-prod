09387bab08ca61a2b1a246d1a19be5b3f22f9bee
smsmi <smsmit40@gmail.com>
2021-01-16 18:05:50 -0600

    all

--- actiondb/settings.py
+++ actiondb/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,5 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
