d1d39c31991eaebeda57cdc2d67f517faccdc808
smsmi <smsmit40@gmail.com>
2021-01-18 16:36:44 -0600

    switched to sqldb

--- actiondb/settings.py
+++ actiondb/settings.py
@@ -77,19 +77,25 @@ WSGI_APPLICATION = 'actiondb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'dgil60515',
+#         'HOST': 'database-1.cw1cbduk9yq8.us-east-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'action_rest_api',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-db_from_evn= dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_evn)
+# db_from_evn= dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'].update(db_from_evn)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
